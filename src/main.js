8cea311e67f67c03afc416088934ea541fcc1535
Nikael Vergara <nikael.marco.vergara@gmail.com>
2017-05-18 16:39:28 +0800

    Enabled sass import

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 
+// import sass files
+import './assets/styles/main.scss';
+
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
