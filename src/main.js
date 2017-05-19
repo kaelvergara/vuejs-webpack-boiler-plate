1b7d9928cdf77fd17ce256fe5b242a8560cbf049
Nikael Vergara <nikael.marco.vergara@gmail.com>
2017-05-19 13:37:31 +0800

    Added bootstrap css library

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import App from './App';
 
+// import vendor css
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import App from './App';
 import router from './router';
 import store from './store';
 
