235ed83e39768d8c3e9c0f6daca241d854867f60
Nikael Vergara <nikael.marco.vergara@gmail.com>
2017-05-23 17:20:31 +0800

    Added basic form with field

--- src/routes/SimpleForm/index.vue
+++ src/routes/SimpleForm/index.vue
@@ -1,9 +1,28 @@
 <template>
-  <span>SimpleForm</span>
+  <div>
+    <span>SimpleForm</span>
+    <form class="mui-form">
+      <legend>Title</legend>
+      <div class="mui-textfield">
+        <input type="text" placeholder="Name" v-model="name">
+      </div>
+      <div class="mui-textfield">
+        <input type="text" placeholder="Last Name" v-model="lastName">
+      </div>
+      <div class="mui-textfield">
+        <textarea placeholder="Textarea"></textarea>
+      </div>
+      <button type="submit" class="mui-btn mui-btn--raised">Submit</button>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'SimpleForm',
+  data: () => ({
+    name: '',
+    lastName: '',
+  }),
 };
 </script>
