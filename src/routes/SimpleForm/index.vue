5b8ee6bb53bff73681a4882772ab87c62b76d2b6
Nikael Vergara <nikael.marco.vergara@gmail.com>
2017-05-24 12:35:22 +0800

    Added initial form with simple validation

--- src/routes/SimpleForm/index.vue
+++ src/routes/SimpleForm/index.vue
@@ -1,28 +1,87 @@
 <template>
   <div>
     <span>SimpleForm</span>
-    <form class="mui-form">
-      <legend>Title</legend>
-      <div class="mui-textfield">
-        <input type="text" placeholder="Name" v-model="name">
+    <form class="form-horizontal">
+      <div class="form-group">
+        <label for="inputPassword3" class="col-sm-2 control-label">Name</label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control" placeholder="name" name="name" v-model="simpleForm.name.value">
+          <span v-show="errors.has('name')" class="validation-error">{{ errors.first('name') }}</span>
+        </div>
       </div>
-      <div class="mui-textfield">
-        <input type="text" placeholder="Last Name" v-model="lastName">
+      <div class="form-group">
+        <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control" placeholder="Email" name="email" v-model="simpleForm.email.value">
+          <span v-show="errors.has('email')" class="validation-error">{{ errors.first('email') }}</span>
+        </div>
       </div>
-      <div class="mui-textfield">
-        <textarea placeholder="Textarea"></textarea>
+      <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+          <div class="checkbox">
+            <label>
+              <input type="checkbox"> Remember me
+            </label>
+          </div>
+        </div>
       </div>
-      <button type="submit" class="mui-btn mui-btn--raised">Submit</button>
+      <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+          <button type="button" class="btn btn-default" v-on:click="validateAll">Sign in</button>
+        </div>
+      </div>
     </form>
   </div>
 </template>
 
 <script>
+import Vue from 'vue';
+import VeeValidate from 'vee-validate';
+
+Vue.use(VeeValidate);
+
 export default {
   name: 'SimpleForm',
   data: () => ({
-    name: '',
-    lastName: '',
+    simpleForm: {
+      email: {
+        value: 'asdfasdf',
+        validations: 'required|email',
+      },
+      name: {
+        value: '',
+        validations: 'required|alpha',
+      },
+    },
   }),
+  methods: {
+    validateAll() {
+      const form = {};
+      Object.keys(this.simpleForm).forEach((field) => {
+        form[field] = this.simpleForm[field].value;
+      });
+
+      this.$validator.validateAll(form).then(() => {
+        console.log('okay');
+      }).catch(() => console.log('error'));
+    },
+  },
+  created() {
+    Object.keys(this.simpleForm).forEach((field) => {
+      // validate field on value change
+      this.$watch(`simpleForm.${field}.value`, (newValue) => {
+        this.$validator.validate(field, newValue);
+      });
+
+      // attach field.
+      this.$validator.attach(field, this.simpleForm[field].validations);
+    });
+  },
 };
 </script>
+
+<style scope>
+.validation-error {
+  color: red;
+}
+</style>
