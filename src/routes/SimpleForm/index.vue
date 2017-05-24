6fdcd96f1b04470a52680228b570511da6df8a99
Nikael Vergara <nikael.marco.vergara@gmail.com>
2017-05-24 17:21:49 +0800

    Added additional field types and disable binding for fields without validations

--- src/routes/SimpleForm/index.vue
+++ src/routes/SimpleForm/index.vue
@@ -3,33 +3,82 @@
     <span>SimpleForm</span>
     <form class="form-horizontal">
       <div class="form-group">
-        <label for="inputPassword3" class="col-sm-2 control-label">Name</label>
+        <label for="name" class="col-sm-2 control-label">Name</label>
         <div class="col-sm-10">
-          <input type="text" class="form-control" placeholder="name" name="name" v-model="simpleForm.name.value">
+          <input name="name"type="text" class="form-control" placeholder="name"  v-model="simpleForm.name.value">
           <span v-show="errors.has('name')" class="validation-error">{{ errors.first('name') }}</span>
         </div>
       </div>
       <div class="form-group">
-        <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+        <label for="email" class="col-sm-2 control-label">Email</label>
         <div class="col-sm-10">
           <input type="text" class="form-control" placeholder="Email" name="email" v-model="simpleForm.email.value">
           <span v-show="errors.has('email')" class="validation-error">{{ errors.first('email') }}</span>
         </div>
       </div>
       <div class="form-group has-feedback">
-        <label for="inputEmail3" class="col-sm-2 control-label">Coupon</label>
+        <label for="coupon" class="col-sm-2 control-label">Coupon</label>
         <div class="col-sm-10">
           <input type="text" class="form-control" placeholder="Coupon" name="coupon" v-model="simpleForm.coupon.value">
-          <span v-show="simpleForm.coupon.isLoading" class="glyphicon glyphicon-refresh glyphicon-refresh-animate form-control-feedback" aria-hidden="true"></span>
+          <span
+            v-show="simpleForm.coupon.isLoading"
+            class="glyphicon glyphicon-refresh glyphicon-refresh-animate form-control-feedback"
+            aria-hidden="true" />
           <span v-show="errors.has('coupon')" class="validation-error">{{ errors.first('coupon') }}</span>
         </div>
       </div>
       <div class="form-group">
-        <div class="col-sm-offset-2 col-sm-10">
-          <div class="checkbox">
-            <label>
-              <input type="checkbox"> Remember me
-            </label>
+        <label for="description" class="col-sm-2 control-label">Description</label>
+        <div class="col-sm-10">
+          <textarea class="form-control" rows="3"  name="description" v-model="simpleForm.description.value"></textarea>
+          <span v-show="errors.has('description')" class="validation-error">{{ errors.first('description') }}</span>
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="gender" class="col-sm-2 control-label">Gender</label>
+        <div class="col-sm-10">
+          <label class="radio-inline">
+            <input type="radio" name="inlineRadioOptions" value="male" v-model="simpleForm.gender.value" /> Male
+          </label>
+          <label class="radio-inline">
+            <input type="radio" name="inlineRadioOptions" value="female" v-model="simpleForm.gender.value" /> Female
+          </label>
+          <div>
+            <span v-show="errors.has('gender')" class="validation-error">{{ errors.first('gender') }}</span>
+          </div>
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="languages" class="col-sm-2 control-label">Languages</label>
+        <div class="col-sm-10">
+          <label class="checkbox-inline">
+            <input type="checkbox" value="Java" v-model="simpleForm.languages.value" />
+            Java
+          </label>
+          <label class="checkbox-inline">
+            <input type="checkbox" value="Javascript" v-model="simpleForm.languages.value" />
+            Javascript
+          </label>
+          <label class="checkbox-inline">
+            <input type="checkbox" value="Ruby" v-model="simpleForm.languages.value" />
+            Ruby
+          </label>
+          <div>
+            <span v-show="errors.has('languages')" class="validation-error">{{ errors.first('languages') }}</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label for="course" class="col-sm-2 control-label">Course</label>
+        <div class="col-sm-10">
+          <select class="form-control" v-model="simpleForm.course.value">
+            <option>IT</option>
+            <option>CS</option>
+            <option>ECE</option>
+          </select>
+          <div>
+            <span v-show="errors.has('course')" class="validation-error">{{ errors.first('course') }}</span>
           </div>
         </div>
       </div>
@@ -55,12 +104,10 @@ export default {
       email: {
         value: '',
         validations: 'required|email',
-        asyncValidation: false,
       },
       name: {
         value: '',
         validations: 'required|alpha',
-        asyncValidation: false,
       },
       coupon: {
         value: '',
@@ -68,13 +115,30 @@ export default {
         asyncValidation: true,
         isLoading: false,
       },
+      description: {
+        value: '',
+      },
+      gender: {
+        value: '',
+        validations: 'required',
+      },
+      languages: {
+        value: [],
+        validations: 'required',
+      },
+      course: {
+        value: '',
+        validations: 'required',
+      },
     },
   }),
   methods: {
     validateAll() {
       const form = {};
       Object.keys(this.simpleForm).forEach((field) => {
-        form[field] = this.simpleForm[field].value;
+        if (this.simpleForm[field].validations) {
+          form[field] = this.simpleForm[field].value;
+        }
       });
 
       this.$validator.validateAll(form).then(() => {
@@ -86,24 +150,27 @@ export default {
   },
   created() {
     Object.keys(this.simpleForm).forEach((field) => {
-      // validate field on value change
-      if (this.simpleForm[field].asyncValidation) {
-        this.$watch(`simpleForm.${field}.value`, (newValue) => {
-          this.simpleForm[field].isLoading = true;
-          this.$validator.validate(field, newValue).then(() => {
-            this.simpleForm[field].isLoading = false;
-          }).catch(() => {
-            this.simpleForm[field].isLoading = false;
+      // listen for values and attach validation if there is
+      if (this.simpleForm[field].validations) {
+        // validate field on value change
+        if (this.simpleForm[field].asyncValidation) {
+          this.$watch(`simpleForm.${field}.value`, (newValue) => {
+            this.simpleForm[field].isLoading = true;
+            this.$validator.validate(field, newValue).then(() => {
+              this.simpleForm[field].isLoading = false;
+            }).catch(() => {
+              this.simpleForm[field].isLoading = false;
+            });
           });
-        });
-      } else {
-        this.$watch(`simpleForm.${field}.value`, (newValue) => {
-          this.$validator.validate(field, newValue);
-        });
-      }
+        } else {
+          this.$watch(`simpleForm.${field}.value`, (newValue) => {
+            this.$validator.validate(field, newValue);
+          });
+        }
 
-      // attach validation to field
-      this.$validator.attach(field, this.simpleForm[field].validations);
+        // attach validation to field
+        this.$validator.attach(field, this.simpleForm[field].validations);
+      }
     });
   },
 };
